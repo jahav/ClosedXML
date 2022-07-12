00730ad8adda20482a4a97e349bef9e7b9453b02
jahav <havlicek.honza@gmail.com>
2022-07-12 17:38:05 +0200

    Improve number of patterns that are recognized by the legacy compatibility layer.

--- ClosedXML/Excel/CalcEngine/CompatibilityFormulaVisitor.cs
+++ ClosedXML/Excel/CalcEngine/CompatibilityFormulaVisitor.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Old parser was using <see cref="XObjectExpression"/> for references. AST represents references through
     /// other nodes. The visitor replaces a subset of <see cref="ReferenceNode"/> patterns with <see cref="XObjectExpression"/>
-    /// so everything works as it did with the old parser.
+    /// so everything works as it did with the old parser. Will be removed once reference operations are implemented.
     /// </summary>
     internal class CompatibilityFormulaVisitor : DefaultFormulaVisitor<object>
     {
@@ -16,20 +16,14 @@
 
         public override Expression Visit(object context, ReferenceNode referenceNode)
         {
-            // Pattern: A1 or SomeNamedRange or A:Z or 1:14
-            if (referenceNode.Prefix is null && (
-                referenceNode.Type == ReferenceItemType.Cell
-                || referenceNode.Type == ReferenceItemType.NamedRange
-                || referenceNode.Type == ReferenceItemType.VRange
-                || referenceNode.Type == ReferenceItemType.HRange))
+            // Pattern: 'A1' or 'SomeNamedRange' or 'A:Z' or '1:14'
+            if (referenceNode.Prefix is null)
             {
                 return new XObjectExpression(_calcEngine.GetExternalObject(referenceNode.Address));
             }
 
-            // Pattern: Sheet!A1
-            if (referenceNode.Prefix.File is null
-            && referenceNode.Prefix.Sheet is not null
-            && referenceNode.Type == ReferenceItemType.Cell)
+            // Pattern: 'Sheet!A1' or 'Sheet!SomeNamedRange' or 'Sheet!A:Z' or 'Sheet!1:14'
+            if (referenceNode.Prefix.File is null && referenceNode.Prefix.Sheet is not null)
             {
                 return new XObjectExpression(_calcEngine.GetExternalObject(referenceNode.Prefix.Sheet.EscapeSheetName() + "!" + referenceNode.Address));
             }
@@ -53,13 +47,27 @@
                 }
 
                 // Pattern Sheet!A1:B2
-                if (leftReference.Prefix.File is null
+                if (leftReference.Prefix is not null
+                    && leftReference.Prefix.File is null
                     && leftReference.Prefix.Sheet is not null
                     && leftReference.Type == ReferenceItemType.Cell
                     && rightReference.Prefix is null
                     && rightReference.Type == ReferenceItemType.Cell)
                 {
                     return new XObjectExpression(_calcEngine.GetExternalObject(leftReference.Prefix.Sheet.EscapeSheetName() + "!" + leftReference.Address + ":" + rightReference.Address));
+                }
+
+                // Pattern SomeSheet!A1:OtherSheet!B2, if from one sheet = OK, otherwise ArgumentOutOfRangeException
+                if (leftReference.Prefix is not null
+                    && leftReference.Prefix.File is null
+                    && leftReference.Prefix.Sheet is not null
+                    && leftReference.Type == ReferenceItemType.Cell
+                    && rightReference.Prefix is not null
+                    && rightReference.Prefix.File is null
+                    && rightReference.Type == ReferenceItemType.Cell
+                    && rightReference.Prefix.Sheet is not null)
+                {
+                    return new XObjectExpression(_calcEngine.GetExternalObject(leftReference.Prefix.Sheet.EscapeSheetName() + "!" + leftReference.Address + ":" + rightReference.Prefix.Sheet.EscapeSheetName() + "!" + rightReference.Address));
                 }
             }
 
